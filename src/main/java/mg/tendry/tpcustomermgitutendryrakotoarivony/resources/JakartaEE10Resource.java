3f25e4779af3d22ddf8d5bd859a8d7afd964f05e
Tendry Arivony <80846844+TendryArivony@users.noreply.github.com>
2024-03-19 22:14:12 +0300

    Etat initial

---
+++ src/main/java/mg/tendry/tpcustomermgitutendryrakotoarivony/resources/JakartaEE10Resource.java
@@ -0,0 +1,20 @@
+package mg.tendry.tpcustomermgitutendryrakotoarivony.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("jakartaee10")
+public class JakartaEE10Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE")
+                .build();
+    }
+}
