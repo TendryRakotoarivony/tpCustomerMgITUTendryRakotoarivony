58cf203f1d91cb7a0158115973d723a557d03a5d
Tendry Rakotoarivony <153612447+TendryRakotoarivony@users.noreply.github.com>
2024-03-28 16:42:37 +0300

    Revert "Mise à jour Plugin et Ajout balise form"
    This reverts commit 887fa6afb861b02ee231813428748c34c7eafcca.

--- src/main/java/mg/tendry/tpcustomermgitutendryrakotoarivony/service/CustomerManager.java
+++ src/main/java/mg/tendry/tpcustomermgitutendryrakotoarivony/service/CustomerManager.java
@@ -31,8 +31,4 @@ public class CustomerManager {
     public void persist(Customer customer) {
        em.persist(customer);
     }
-
-    public Customer findById(int idCustomer) {  
-        return em.find(Customer.class, idCustomer);  
-    }
 }
